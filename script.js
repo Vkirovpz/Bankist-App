bef779ab36542e0f9d9c313c35857ff8acd2440b
Vkirovpz <Vkirovpz@students.softuni.bg>
2023-12-03 22:55:40 +0200

    Reveal sections with IntersectionObserver

--- script.js
+++ script.js
@@ -98,3 +98,22 @@ const handleOver = function (e) {
 
 nav.addEventListener('mouseover', handleOver.bind(0.5));
 nav.addEventListener('mouseout', handleOver.bind(1.0));
+
+const allSections = document.querySelectorAll('.section');
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
